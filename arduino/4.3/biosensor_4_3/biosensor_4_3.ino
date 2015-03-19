6ea677d4d2d324d7c818dca1922cdc51820ce66b
Elzabad Kennedy <kenned45@illinois.edu>
2015-03-19 08:41:59 -0500

    #17 Created the readADC() function, did some refactoring of magic numbers,  and started structuring the combined ino

--- arduino/4.3/biosensor_4_3/biosensor_4_3.ino
+++ arduino/4.3/biosensor_4_3/biosensor_4_3.ino
@@ -1,1 +1,39 @@
 
+
+
+
+
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+#include <SPI.h>
+
+#define MULTIPLIER 0.125F   // ADS1115  1x gain   +/- 4.096V (16-bit results) 0.125mV
+#define RESISTANCE 98.2     // @TODO
+
+
+Adafruit_ADS1115 ads;
+
+
+
+double anodePotential;
+double current;
+double cell_vol;
+double vol;
+
+setup()
+{
+    
+}
+
+loop()
+{
+    
+}
+
+void readADC()
+{
+    vol = (ads.readADC_Differential_0_1())*MULTIPLIER;  // Voltage reading
+    current = ((vol)/(RESISTANCE));  // ohm's law
+    anodePotential = ((ads.readADC_Differential_2_3())*MULTIPLIER)/1000;
+    cell_vol = ((ads.readADC_SingleEnded(1))*MULTIPLIER)/1000;
+}
